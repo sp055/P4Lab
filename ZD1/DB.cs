9203bd70447644a38feb3f4186064860d5d57d34
sp055 <sp055@DESKTOP-4RJTAR6>
2021-04-20 14:19:18 +0200

    v2

---
+++ ZD1/DB.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ZD1
+{
+    public class DB
+    {
+        private SqlConnection polaczenie;
+        public DB(string connectionString)
+        {
+            polaczenie = new SqlConnection(connectionString);
+        }
+
+        public void Create() //wpisywanie/tworzenie danych
+        {
+            polaczenie.Open();
+
+            string idKlienta;
+            string nazwaFirmy;
+            Console.Write("Podaj IdKlienta (max 5znaków): ");
+            idKlienta = Console.ReadLine();
+            Console.Write("Podaj nazwe firmy: ");
+            nazwaFirmy = Console.ReadLine();
+
+            if ((idKlienta.Length & nazwaFirmy.Length) > 0)
+            {
+                var insertSql = "INSERT INTO dbo.Klienci (IDklienta, NazwaFirmy) VALUES (@ID, @NazwaFirmy)"; // zakaz wpisywania danych na sztywno dlatego używamy zmiennych @
+                var insertCommand = new SqlCommand(insertSql, polaczenie);
+                insertCommand.Parameters.Add(new SqlParameter("@ID", idKlienta));
+                insertCommand.Parameters.Add(new SqlParameter("@NazwaFirmy", nazwaFirmy));
+                insertCommand.ExecuteNonQuery();
+            }
+            else
+            {
+                Console.WriteLine("Nie podałeś nazwy.");
+            }
+
+            polaczenie.Close();
+        }
+
+        
+        public void Read() //czytanie danych
+        {
+            polaczenie.Open();
+
+            var selectSql = "SELECT * FROM dbo.Klienci";
+            var command = new SqlCommand(selectSql, polaczenie);
+            var reader = command.ExecuteReader();
+            foreach (DbDataRecord item in reader)
+            {
+                Console.WriteLine(item.GetString(0) + " " + item.GetString(1));
+            }
+            Console.WriteLine();
+
+            polaczenie.Close();
+        }
+
+        public void Update() //aktualizowanie danych
+        {
+            polaczenie.Open();
+
+            string idKlientaUpdate;
+            string nazwaFirmyUpdate;
+            Console.Write("Podaj IDklienta którego nazwe firmy chcesz zaaktualizować: ");
+            idKlientaUpdate = Console.ReadLine();
+            Console.Write("Jak ma się nazywać zmieniona firma? ");
+            nazwaFirmyUpdate = Console.ReadLine();
+
+            var updateSql = $"UPDATE dbo.Klienci SET NazwaFirmy = @NazwaFirmy WHERE IDklienta = @ID";
+            var updateCommand = new SqlCommand(updateSql, polaczenie);
+            updateCommand.Parameters.Add(new SqlParameter("@NazwaFirmy", nazwaFirmyUpdate));
+            updateCommand.Parameters.Add(new SqlParameter("@ID", idKlientaUpdate));
+            updateCommand.ExecuteNonQuery();
+
+            polaczenie.Close();
+        }
+
+        public void Delete() //usuwanie danych
+        {
+            polaczenie.Open();
+
+            string idKlientaDelete;
+            Console.Write("Podaj IDKlienta którego firmę chcesz usunąć: ");
+            idKlientaDelete = Console.ReadLine();
+
+            var deleteSql = $"DELETE FROM dbo.Klienci WHERE IDklienta = @ID";
+            var deleteCommand = new SqlCommand(deleteSql, polaczenie);
+            deleteCommand.Parameters.Add(new SqlParameter("@ID", idKlientaDelete));
+            deleteCommand.ExecuteNonQuery();
+
+            polaczenie.Close();
+        }
+    }
+}
--- ZD1/Program.cs
+++ ZD1/Program.cs
@@ -11,68 +11,19 @@ namespace ZD1
             var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=P4;Integrated Security=True;" +
                                     "Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;" +
                                     "MultiSubnetFailover=False;MultipleActiveResultSets = True";
-            using var connection = new SqlConnection(connectionString);
 
-            connection.Open(); //otwarcie polaczenia
+            var db = new DB(connectionString);
 
-            var selectSql = "SELECT * FROM dbo.Klienci";
-            var command = new SqlCommand(selectSql, connection);
+            db.Create();
+            db.Read();
+            Console.WriteLine("---------------");
+            db.Update();
+            //dataBaseObj.Read();
+            Console.WriteLine("---------------");
+            db.Delete();
+            //dataBaseObj.Read();
+            Console.WriteLine("---------------");
 
-            var reader = command.ExecuteReader();
-
-            //czytanie danych
-            foreach (DbDataRecord item in reader)
-            {
-                Console.WriteLine(item.GetString(0) + " " + item.GetString(1));
-            }
-            Console.WriteLine();
-
-            //wpisywanie danych
-            string idKlienta;
-            string nazwaFirmy;
-            Console.Write("Podaj IdKlienta (max 5znaków): ");
-            idKlienta = Console.ReadLine();
-            Console.Write("Podaj nazwe firmy: ");
-            nazwaFirmy = Console.ReadLine();
-
-            if ((idKlienta.Length & nazwaFirmy.Length) > 0)
-            {
-                var insertSql = "INSERT INTO dbo.Klienci (IDklienta, NazwaFirmy) VALUES (@ID, @NazwaFirmy)"; // zakaz wpisywania danych na sztywno dlatego używamy zmiennych @
-                var insertCommand = new SqlCommand(insertSql, connection);
-                insertCommand.Parameters.Add(new SqlParameter("@ID", idKlienta));
-                insertCommand.Parameters.Add(new SqlParameter("@NazwaFirmy", nazwaFirmy));
-                insertCommand.ExecuteNonQuery();
-            }
-            else
-            {
-                Console.WriteLine("Nie podałeś nazwy.");
-            }
-
-            //aktualizowanie danych
-            string idKlientaUpdate;
-            string nazwaFirmyUpdate;
-            Console.Write("Podaj IDklienta którego nazwe firmy chcesz zaaktualizować: ");
-            idKlientaUpdate = Console.ReadLine();
-            Console.Write("Jak ma się nazywać zmieniona firma? ");
-            nazwaFirmyUpdate = Console.ReadLine();
-
-            var updateSql = $"UPDATE dbo.Klienci SET NazwaFirmy = @NazwaFirmy WHERE IDklienta = @ID";
-            var updateCommand = new SqlCommand(updateSql, connection);
-            updateCommand.Parameters.Add(new SqlParameter("@NazwaFirmy", nazwaFirmyUpdate));
-            updateCommand.Parameters.Add(new SqlParameter("@ID", idKlientaUpdate));
-            updateCommand.ExecuteNonQuery();
-
-            //usuwanie danych
-            string idKlientaDelete;
-            Console.Write("Podaj IDKlienta którego firmę chcesz usunąć: ");
-            idKlientaDelete = Console.ReadLine();
-
-            var deleteSql = $"DELETE FROM dbo.Klienci WHERE IDklienta = @ID";
-            var deleteCommand = new SqlCommand(deleteSql, connection);
-            deleteCommand.Parameters.Add(new SqlParameter("@ID", idKlientaDelete));
-            deleteCommand.ExecuteNonQuery();
-
-            connection.Close(); // trzeba zamykać ręcznie połączenie żeby nie obciążać bazy danych
         }
     }
 }
