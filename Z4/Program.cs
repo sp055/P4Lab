fb12beb4692611aea660d20ce08ebcb6d26193b6
sp055 <sp055@DESKTOP-4RJTAR6>
2021-04-29 14:55:27 +0200

    v1.1

--- Z4/Program.cs
+++ Z4/Program.cs
@@ -31,10 +31,15 @@ namespace Z4
             //tph.Computers.Add(new Laptop() { Price = 800, Weight = 4.5 });
             //tph.SaveChanges();
 
-            var tpt = new TPTContext();
-            tpt.Computers.Add(new PC() { Price = 1000, CoolingType = "Air" });
-            tpt.Computers.Add(new Laptop() { Price = 800, Weight = 4.5 });
-            tpt.SaveChanges();
+            //var tpt = new TPTContext();
+            //tpt.Computers.Add(new PC() { Price = 1000, CoolingType = "Air" });
+            //tpt.Computers.Add(new Laptop() { Price = 800, Weight = 4.5 });
+            //tpt.SaveChanges();
+
+            var tpc = new TPCContext();
+            tpc.Computers.Add(new PC() { Id = 1, Price = 1000, CoolingType = "Air" });
+            tpc.Computers.Add(new Laptop() { Id = 2, Price = 800, Weight = 4.5 });
+            tpc.SaveChanges();
 
             Console.WriteLine("Done");
             Console.ReadLine();
---
+++ Z4/TPCContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z4
+{
+    public class TPCContext : DbContext
+    {
+        public DbSet<Computer> Computers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PC>().Map(x => x.MapInheritedProperties().ToTable("Pecety"));
+            modelBuilder.Entity<Laptop>().Map(x => x.MapInheritedProperties().ToTable("Laptopy"));
+        }
+    }
+}
