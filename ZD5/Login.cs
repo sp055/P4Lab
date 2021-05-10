c2d546538b42851d9ef939ca2927178b8a7b6380
sp055 <sp055@DESKTOP-4RJTAR6>
2021-05-10 19:55:18 +0200

    v1.1

--- ZD5/Login.cs
+++ ZD5/Login.cs
@@ -17,16 +17,18 @@ namespace ZD5
             InitializeComponent();
             this.FormClosed +=
                new System.Windows.Forms.FormClosedEventHandler(this.LoginWindowClosed);
+            // dzięki temu po kliknięciu X w okonie Logowania aplikacja się zamknie a nie będzie wisiała w pamięci
         }
 
         private void LoginWindowClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+            //dalsza część
         }
 
-        private void bZaloguj_Click(object sender, EventArgs e)
+        private void bZaloguj_Click(object sender, EventArgs e) // przy kliknięciu sprawdza podane dane z już istniejącymi
         {
-            foreach (var user in User.UserList)
+            foreach (var user in User.UserList) 
             {
                 //jesli wszystko sie zgadza
                 if (user.Username == txtUsername.Text & user.Password == txtPassword.Text)
--- ZD5/Rejestracja.cs
+++ ZD5/Rejestracja.cs
@@ -15,45 +15,47 @@ namespace ZD5
         public Rejestracja()
         {
             InitializeComponent();
-            User admin = new User("admin", "admin");
-            User.AddUserToList(admin);
+            User admin = new User("admin", "admin"); // testowy użytkownik który zawsze się pojawia przy starcie programu żeby
+            User.AddUserToList(admin);               // zaprezentować funkcje szukania użytkownika w oknie Logowanie
         }
 
-        private void BZarejestruj_Click(object sender, EventArgs e)
+        private void BZarejestruj_Click(object sender, EventArgs e) //przy kliknięciu guzika odpala sprawdzanie
         {
-            if (string.IsNullOrWhiteSpace(txtUsername.Text))
+            if (string.IsNullOrWhiteSpace(txtUsername.Text)) //brak nazwy
             {
-                MessageBox.Show("Podaj nazwe użytkownika.");
+                MessageBox.Show("Podaj nazwe użytkownika."); 
             }
-            else if(SprawdzCzyIstnieje(txtUsername.Text))
+            else if(SprawdzCzyIstnieje(txtUsername.Text)) //taki użytkownik już jest - w tym wypadku 'admin'
             {
                 MessageBox.Show("Taki użytkownik już istnieje.");
             }
-            else if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            else if (string.IsNullOrWhiteSpace(txtPassword.Text)) //puste haslo
             {
                 MessageBox.Show("Hasło nie może być puste.");
             }
-            else if (string.IsNullOrWhiteSpace(txtReapetPassword.Text) || !(txtPassword.Text == txtReapetPassword.Text))
+            else if (string.IsNullOrWhiteSpace(txtReapetPassword.Text) || !(txtPassword.Text == txtReapetPassword.Text)) //hasla sie nie zgadzaja
             {
                 MessageBox.Show("Hasła nie są takie same.");
             }
-            else if (!cbRODO.Checked)
+            else if (!cbRODO.Checked) //nie kliknieto checkboxa
             {
                 MessageBox.Show("Musisz zaakceptować klauzulę RODO.");
             }
             else
-            {
+            {   
+                //tworzy użytkownika z podanych wcześniej danych i przesyła dane do listy żeby zapisać je dla okna Logowanie
                 MessageBox.Show("Pomyślnie zarejestrowano. Możesz się teraz zalogować.");
                 User user = new User(txtUsername.Text, txtPassword.Text);
                 User.AddUserToList(user);
 
+                //otwiera okno Logowanie i chowa okno Rejestracja
                 Login oknoLogowania = new Login();
                 oknoLogowania.Show();
                 this.Hide();
             }
         }
 
-        static bool SprawdzCzyIstnieje(string a)
+        static bool SprawdzCzyIstnieje(string a) // funkcja pomocna do sprawdzenia czy taki użytkownik już istnieje
         {
             bool sprawdz;
             foreach (var user in User.UserList)
