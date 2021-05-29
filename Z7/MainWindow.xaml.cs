8b5733829c31a346c316d4b8f4da50d0124f2929
sp055 <sp055@DESKTOP-4RJTAR6>
2021-05-29 11:16:26 +0200

    final

---
+++ Z7/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Z7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Label label;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label = new Label() { Content = "TEST" };
+            MyWrapPanel.Children.Add(label);
+            MyWrapPanel.Children.OfType<Button>().Where(x => x.Content.ToString() == "Button");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MyWrapPanel.Children.Clear();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            label.Content = "XAML";
+        }
+    }
+}
